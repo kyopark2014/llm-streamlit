1bee2d870f9996b25db95b6f100d4dcc86520445
John Park <kyopark2014@gmail.com>
2024-12-27 17:53:00 +0900

    update

--- cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
+++ cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
@@ -135,7 +135,7 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
       ]
     });  
 
-  /*  const s3BucketAcessPoint = vpc.addGatewayEndpoint(`s3Endpoint-${projectName}`, {
+    const s3BucketAcessPoint = vpc.addGatewayEndpoint(`s3Endpoint-${projectName}`, {
       service: ec2.GatewayVpcEndpointAwsService.S3,
     });
 
@@ -145,7 +145,7 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
         actions: ['s3:*'],
         resources: ['*'],
       }),
-    ); */
+    ); 
 
     // EC2 Security Group
     const ec2Sg = new ec2.SecurityGroup(this, `ec2-sg-for-${projectName}`,
