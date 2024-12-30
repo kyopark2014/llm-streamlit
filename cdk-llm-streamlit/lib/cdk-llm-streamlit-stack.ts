6e0ca69cbdccda4c41184fda00323d68b20cc67f
John Park <kyopark2014@gmail.com>
2024-12-30 22:01:28 +0900

    update

--- cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
+++ cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
@@ -117,7 +117,7 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
     // vpc
     const vpc = new ec2.Vpc(this, `vpc-for-${projectName}`, {
       vpcName: `vpc-for-${projectName}`,
-      maxAzs: 2,
+      maxAzs: 4,
       ipAddresses: ec2.IpAddresses.cidr("20.64.0.0/16"),
       natGateways: 1,
       createInternetGateway: true,
