1d402896a6b74d1c05807a8359d6445d7303649d
John Park <kyopark2014@gmail.com>
2025-01-02 14:54:22 +0900

    update

--- cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
+++ cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
@@ -282,11 +282,12 @@ EOF"`,
 port=${targetPort}
 
 [theme]
-base="dark
+base="dark"
+primaryColor="#fff700"
 EOF"`,
       `json='${JSON.stringify(environment)}' && echo "$json">/home/config.json`,      
       `runuser -l ec2-user -c 'cd && git clone https://github.com/kyopark2014/${projectName}'`,
-      `runuser -l ec2-user -c 'pip install streamlit streamlit_chat watchtower'`,        
+      `runuser -l ec2-user -c 'pip install streamlit streamlit_chat'`,        
       `runuser -l ec2-user -c 'pip install boto3 langchain_aws langchain langchain_community langgraph'`,
       `runuser -l ec2-user -c 'pip install beautifulsoup4 pytz tavily-python'`,
       'systemctl enable streamlit.service',
