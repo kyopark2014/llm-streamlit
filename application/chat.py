6070d0b1caaaef4be92b68bc7ef64884c8d2b284
John Park <kyopark2014@gmail.com>
2025-01-13 08:41:18 +0900

    update

--- application/chat.py
+++ application/chat.py
@@ -233,7 +233,7 @@ try:
         include_raw_content=True,
         api_wrapper=tavily_api_wrapper,
         search_depth="advanced", # "basic"
-        include_domains=["google.com", "naver.com"]
+        # include_domains=["google.com", "naver.com"]
     )
     output = search.invoke(query)
     print('tavily output: ', output)
