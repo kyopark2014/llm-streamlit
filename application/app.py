72a0a0853f1697979ff1c670b8e75804492b6294
John Park <kyopark2014@gmail.com>
2025-02-25 08:57:22 +0900

    update

--- application/app.py
+++ application/app.py
@@ -65,10 +65,10 @@ with st.sidebar:
     if mode == '이미지 분석':
         index = 3
     else:
-        index = 0   
+        index = 3   
     modelName = st.selectbox(
         '🖊️ 사용 모델을 선택하세요',
-        ('Nova Pro', 'Nova Lite', 'Nova Micro', 'Claude 3.5 Sonnet', 'Claude 3.0 Sonnet', 'Claude 3.5 Haiku'), index=index
+        ('Nova Pro', 'Nova Lite', 'Nova Micro', 'Claude 3.7 Sonnet', 'Claude 3.5 Sonnet', 'Claude 3.0 Sonnet', 'Claude 3.5 Haiku'), index=index
     )
     
     uploaded_file = None
