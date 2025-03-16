540259162387c870d3ad7992fad0b1429c7c1e74
John Park <kyopark2014@gmail.com>
2025-03-16 18:36:32 +0900

    update

--- cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
+++ cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
@@ -316,7 +316,7 @@ EOF"`,
       `runuser -l ec2-user -c 'cd && git clone https://github.com/kyopark2014/${projectName}'`,
       `runuser -l ec2-user -c 'pip install streamlit streamlit_chat'`,        
       `runuser -l ec2-user -c 'pip install boto3 langchain_aws langchain langchain_community langgraph langchain_experimental'`,
-      `runuser -l ec2-user -c 'pip install beautifulsoup4 pytz tavily-python yfinance rizaio plotly_express'`,
+      `runuser -l ec2-user -c 'pip install beautifulsoup4 pytz tavily-python yfinance rizaio plotly_express numpy matplotlib'`,
       'systemctl enable streamlit.service',
       'systemctl start streamlit',      
       `runuser -l ec2-user -c 'cat <<EOF > /home/ec2-user/.streamlit/config.toml
